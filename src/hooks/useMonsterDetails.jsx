3901e9dae6f83e1e6bc66860d4a8868b957fa01a
JPEdgar <Jon.P.Edgar@gmail.com>
2023-04-29 16:36:29 -0700

    Did some minor fixes on data for current/max hp

--- src/hooks/useMonsterDetails.jsx
+++ src/hooks/useMonsterDetails.jsx
@@ -4,7 +4,18 @@ import useMonsterContext from "./context/useMonsterContext";
 
 const useMonsterDetails = () => {
   const { monsterState, setMonsterState } = useMonsterContext();
-  return { monsterState, setMonsterState };
+
+  const healMonster = () => {
+    const maxHP = monsterState.maxHP;
+    setMonsterState((curr) => ({ ...curr, currentHP: maxHP }));
+  };
+
+  const damageMonster = (incomingDamage) => {
+    const newHP = monsterState.currentHP - incomingDamage;
+    setMonsterState((curr) => ({ ...curr, currentHP: newHP }));
+  };
+
+  return { monsterState, healMonster, damageMonster };
 };
 
 export default useMonsterDetails;
