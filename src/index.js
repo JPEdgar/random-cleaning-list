63cc97fc02cb367d5e3d113dfc4201b8ab048f94
JPEdgar <Jon.P.Edgar@gmail.com>
2023-05-01 11:58:50 -0700

    Added styles boiler plate

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import GlobalWrapper from "./context/GlobalWrapper";
+
+import "./styles/styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
