0b2e0993853ca12f71606c1fa19644fcf9531464
JPEdgar <Jon.P.Edgar@gmail.com>
2023-05-01 10:57:14 -0700

    Added lorem image for development

--- src/components/TaskList/Monster.jsx
+++ src/components/TaskList/Monster.jsx
@@ -12,11 +12,12 @@ const Monster = () => {
       <Row className="d-flex align-items-center justify-content-center">
         <Image
           style={{ height: "300px", width: "auto" }}
-          src={
-            monsterDetails.currentDamage >= monsterDetails.maxHP
-              ? monsterDetails.dead
-              : monsterDetails.image
-          }
+          // src={
+          //   monsterDetails.currentDamage >= monsterDetails.maxHP
+          //     ? monsterDetails.dead
+          //     : monsterDetails.image
+          // }
+          src="https://picsum.photos/200"
         />
         <h1 className="text-center">{monsterDetails.name}</h1>
         <h3 className="text-center">{monsterDetails.hp}</h3>
