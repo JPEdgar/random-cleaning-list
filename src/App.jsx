804846331754a1f415b0fafa89bcd319e10880db
JPEdgar <Jon.P.Edgar@gmail.com>
2023-05-26 13:27:53 -0700

    Reset on project, old project in archive

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,9 @@
-import React, { useState, useEffect } from "react";
-
-import { Container } from "react-bootstrap";
-
-import Title from "./components/Title";
-import MonsterHeader from "./components/Monster/MonsterHeader";
-import MonsterHealth from "./components/Monster/MonsterHealth";
-import Tasklist from "./components/Tasklist";
-
-import { useTaskDetails } from "./hooks";
+import React from 'react'
 
 const App = () => {
-  const { tasklist, initialDamage } = useTaskDetails();
-  const [initialBossDamage, setInitialBossDamage] = useState(0);
-
-  useEffect(() => {
-    if (initialDamage.hasInitialDamage)
-      setInitialBossDamage(initialDamage.damage);
-    else setInitialBossDamage(0);
-  }, [initialDamage]);
-
   return (
-    <>
-      <button onClick={() => console.log(tasklist)}>Log task list</button>
-      <Container>
-        <Title />
-        <MonsterHeader />
-        <Tasklist />
-        <MonsterHealth initialDamage={initialBossDamage} />
-      </Container>
-    </>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
