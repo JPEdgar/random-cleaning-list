ca5d70622911ed2e1d07485fba08d8492ed2978a
JPEdgar <Jon.P.Edgar@gmail.com>
2023-08-01 10:41:52 -0700

    Made select monster card into Col for responsive

--- src/components/Monster/AssignMonster.jsx
+++ src/components/Monster/AssignMonster.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Row, Col } from "react-bootstrap";
+
 import SelectMonsterCard from "./SelectMonsterCard";
 
 import { useMonsterList } from "../../hooks";
@@ -7,12 +9,16 @@ import { useMonsterList } from "../../hooks";
 const AssignMonster = () => {
   const { monsterList } = useMonsterList();
 
-  return monsterList.map((monsterData) => (
-    <SelectMonsterCard
-      key={`select-monster-card-${monsterData.id}`}
-      monsterData={monsterData}
-    />
-  ));
+  return (
+    <Row>
+      {monsterList.map((monsterData) => (
+        <SelectMonsterCard
+          key={`select-monster-card-${monsterData.id}`}
+          monsterData={monsterData}
+        />
+      ))}
+    </Row>
+  );
 };
 
 export default AssignMonster;
--- src/components/Monster/SelectMonsterCard.jsx
+++ src/components/Monster/SelectMonsterCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Card } from "react-bootstrap";
+import { Card, Col } from "react-bootstrap";
 
 import EditIcon from "../elements/EditIcon";
 import TrashIcon from "../elements/TrashIcon";
@@ -12,40 +12,45 @@ const SelectMonsterCard = ({ monsterData = {} }) => {
   const { deleteMonster } = useMonsterList();
 
   return (
-    <Card style={{ width: "18rem", cursor: "pointer" }} className="monsterCard">
-      <div>
-        <Card.Img
-          variant="top"
-          src={monsterData.image}
-          onClick={() => assignMonster(monsterData)}
-        />
+    <Col className="my-2">
+      <Card
+        style={{ width: "18rem", cursor: "pointer" }}
+        className="monsterCard"
+      >
+        <div>
+          <Card.Img
+            variant="top"
+            src={monsterData.image}
+            onClick={() => assignMonster(monsterData)}
+          />
 
-        <div
-          style={{
-            position: "absolute",
-            display: "flex",
-            right: "0",
-            top: "0",
-            backgroundColor: "rgba(255, 255, 255, 0.6)",
-            borderRadius: "0 5px 0 5px ",
-          }}
-        >
-          <EditIcon
-            modalType={MODAL_TYPES.EDIT_MONSTER_MODAL}
-            data={monsterData}
-          />
-          <TrashIcon
-            monsterFlag
-            data={{ id: monsterData.id, name: monsterData.name }}
-            deleteCommand={deleteMonster}
-          />
+          <div
+            style={{
+              position: "absolute",
+              display: "flex",
+              right: "0",
+              top: "0",
+              backgroundColor: "rgba(255, 255, 255, 0.6)",
+              borderRadius: "0 5px 0 5px ",
+            }}
+          >
+            <EditIcon
+              modalType={MODAL_TYPES.EDIT_MONSTER_MODAL}
+              data={monsterData}
+            />
+            <TrashIcon
+              monsterFlag
+              data={{ id: monsterData.id, name: monsterData.name }}
+              deleteCommand={deleteMonster}
+            />
+          </div>
         </div>
-      </div>
-      <Card.Body onClick={() => assignMonster(monsterData)}>
-        <Card.Title>{monsterData.name}</Card.Title>
-        <Card.Text>Max Hit Points: {monsterData.maxHP}</Card.Text>
-      </Card.Body>
-    </Card>
+        <Card.Body onClick={() => assignMonster(monsterData)}>
+          <Card.Title>{monsterData.name}</Card.Title>
+          <Card.Text>Max Hit Points: {monsterData.maxHP}</Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
   );
 };
 
