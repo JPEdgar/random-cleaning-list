7a7c67a1b5c560b182b95cf67936a36a7f65a98d
JPEdgar <Jon.P.Edgar@gmail.com>
2023-08-01 13:02:11 -0700

    Refactored index and created add and edit modals

---
+++ src/components/Monster/MonsterModal/AddModal.jsx
@@ -0,0 +1,141 @@
+import React, { useState, useEffect } from "react";
+
+import { Modal, Form, Button, ButtonGroup } from "react-bootstrap";
+
+import { useModalInfo } from "../../../hooks";
+import { createId } from "../../../utilities";
+
+const AddModal = () => {
+  const {
+    hideModal,
+    isModalOpenFlag,
+    modalData = { name: "", maxHP: 0, image: "" },
+    handleChange_MonsterModal,
+    resetModalData,
+    updateMonsterData,
+    saveAndClose,
+  } = useModalInfo();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    handleChange_MonsterModal(e);
+  };
+
+  const handleReset = (id) => {
+    resetModalData(id);
+  };
+
+  const handleSave = (modalData) => {
+    updateMonsterData(modalData);
+  };
+
+  const handleClose = () => {
+    hideModal();
+  };
+
+  const handleSaveAndClose = (modalData) => {
+    saveAndClose(modalData);
+  };
+
+  return (
+    <Modal
+      show={isModalOpenFlag}
+      onHide={() => hideModal()}
+      size="lg"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Add/Create Monster</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3">
+            <Form.Label>Monster Name:</Form.Label>
+            <Form.Control
+              name="name"
+              placeholder="Monster name"
+              value={modalData?.name}
+              onChange={handleChange}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Monster Max Health:</Form.Label>
+            <Form.Control
+              name="maxHP"
+              type="number"
+              placeholder="Monster name"
+              value={modalData?.maxHP}
+              onChange={handleChange}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Monster Online Image Link:</Form.Label>
+            <Form.Control
+              name="image"
+              placeholder="Monster image url"
+              value={modalData?.image}
+              onChange={handleChange}
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        {/* <Button onClick={props.onHide}>Close</Button> */}
+        {/* <button onClick={() => console.log("modalData = ", modalData)}>Log modal data</button> */}
+        <div className="d-flex justify-content-between w-100">
+          <Button
+            size="sm"
+            variant="warning"
+            style={{ width: "100px" }}
+            onClick={() => handleReset(modalData.id)}
+          >
+            Reset
+          </Button>
+          <div>
+            <ButtonGroup className="mx-2">
+              <Button
+                variant="primary"
+                size="sm"
+                style={{ width: "80px" }}
+                onClick={() => handleSave(modalData)}
+              >
+                Save
+              </Button>
+              <Button
+                variant="info"
+                size="sm"
+                style={{ width: "120px" }}
+                onClick={() => handleSaveAndClose(modalData)}
+              >
+                Save and Close
+              </Button>
+            </ButtonGroup>
+
+            <Button
+              size="sm"
+              variant="secondary"
+              style={{ width: "100px" }}
+              onClick={() => handleClose()}
+            >
+              Close
+            </Button>
+          </div>
+        </div>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default AddModal;
+
+/*
+    id: String,
+    name: String,
+    maxHP: Int,
+    image: String,
+*/
--- src/components/Monster/MonsterModal/index.jsx
+++ src/components/Monster/MonsterModal/index.jsx
@@ -1,141 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-import { Modal, Form, Button, ButtonGroup } from "react-bootstrap";
+import EditModal from "./EditModal";
+import AddModal from "./AddModal";
 
+import { MODAL_TYPES } from "../../../constants/types";
 import { useModalInfo } from "../../../hooks";
 
 const MonsterModal = () => {
-  const {
-    hideModal,
-    isModalOpenFlag,
-    modalData,
-    handleChange_MonsterModal,
-    resetModalData,
-    updateMonsterData,
-    saveAndClose,
-  } = useModalInfo();
+const {activeModal} = useModalInfo()
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-
-  const handleChange = (e) => {
-    handleChange_MonsterModal(e);
-  };
-
-  const handleReset = (id) => {
-    resetModalData(id);
-  };
-
-  const handleSave = (modalData) => {
-    updateMonsterData(modalData);
-  };
-
-  const handleClose = () => {
-    hideModal();
-  };
-
-  const handleSaveAndClose = (modalData) => {
-    saveAndClose(modalData);
-  };
 
   return (
-    <Modal
-      show={isModalOpenFlag}
-      onHide={() => hideModal()}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title>Edit monster</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3">
-            <Form.Label>Monster Name:</Form.Label>
-            <Form.Control
-              name="name"
-              placeholder="Monster name"
-              value={modalData?.name}
-              onChange={handleChange}
-            />
-          </Form.Group>
-
-          <Form.Group className="mb-3">
-            <Form.Label>Monster Max Health:</Form.Label>
-            <Form.Control
-              name="maxHP"
-              type="number"
-              placeholder="Monster name"
-              value={modalData?.maxHP}
-              onChange={handleChange}
-            />
-          </Form.Group>
-
-          <Form.Group className="mb-3">
-            <Form.Label>Monster Online Image Link:</Form.Label>
-            <Form.Control
-              name="image"
-              placeholder="Monster image url"
-              value={modalData?.image}
-              onChange={handleChange}
-            />
-          </Form.Group>
-        </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        {/* <Button onClick={props.onHide}>Close</Button> */}
-        {/* <button onClick={() => console.log("modalData = ", modalData)}>Log modal data</button> */}
-        <div className="d-flex justify-content-between w-100">
-          <Button
-            size="sm"
-            variant="warning"
-            style={{ width: "100px" }}
-            onClick={() => handleReset(modalData.id)}
-          >
-            Reset
-          </Button>
-          <div>
-            <ButtonGroup className="mx-2">
-              <Button
-                variant="primary"
-                size="sm"
-                style={{ width: "80px" }}
-                onClick={() => handleSave(modalData)}
-              >
-                Save
-              </Button>
-              <Button
-                variant="info"
-                size="sm"
-                style={{ width: "120px" }}
-                onClick={() => handleSaveAndClose(modalData)}
-              >
-                Save and Close
-              </Button>
-            </ButtonGroup>
-
-            <Button
-              size="sm"
-              variant="secondary"
-              style={{ width: "100px" }}
-              onClick={() => handleClose()}
-            >
-              Close
-            </Button>
-          </div>
-        </div>
-      </Modal.Footer>
-    </Modal>
+    <>
+      {activeModal === MODAL_TYPES.EDIT_MONSTER_MODAL && <EditModal />}
+      {activeModal === MODAL_TYPES.ADD_MONSTER_MODAL && <AddModal />}
+    </>
   );
 };
 
 export default MonsterModal;
-
-/*
-    id: String,
-    name: String,
-    maxHP: Int,
-    image: String,
-*/
